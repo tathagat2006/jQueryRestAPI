bfabce78a690ffd0bcacf118a23a70f8f6445d10
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-08-08 19:03:26 +0530

    user tab working
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function getUsers() {
+function getUsers(done) {
     $.getJSON('http://jsonplaceholder.typicode.com/users' , (data) => {
-        console.log(data)
+        // console.log(data)
+        done(data)
     })
 }
 
 $(function() {
 
+    $('#tab-users').click(() => {
+        getUsers((users) => {
+            users.forEach((user) => {
+                $('#row-users').append(
+                    `
+                        <div class = "card col-3 p-1 m-1">
+                            <div class = "card-body">
+                            <h5 class = "card-title">
+                                ${user.name}
+                            </h5>
+                            <h6 class = "card-subtitle">${user.email}</h6>
+                            <p class = "card-text">
+                                {user.address.street}<br>
+                                {user.address.suite}<br>
+                                {user.address.city}<br>
+                                {user.address.zipcode}<br>
+                            </p>
+                            </div>
+                        </div>
+                    `
+                )
+            })
+        })
+    })
 })
